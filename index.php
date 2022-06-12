bdf991e477ec763980a7ef42f0bedfe7b03bac4c
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-12 19:01:24 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+
+	<!-- Mobile -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Away</title>
+
+	<!-- Bootstrap CSS Only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+	<!-- Own Style Sheet -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+
+</head>
+	<body>
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 offset-md-3">
+
+						<h1>Hello!!! Welcome Here</h1>
+
+							<form action="" method=""></form>
+
+							<div>
+
+								<label>Username</label>
+								<br>
+								<input type="text" name="fullname">
+							
+							</div>
+
+							<div>
+
+								<label>Email</label>
+								<br>
+								<input type="email" name="emailadress">
+							
+							</div>
+
+							<div>
+
+								<label>Messege</label>
+								<br>
+								<input type="text" name="fullname">
+							
+							</div>
+
+							<div>
+								<br>
+
+								<button type="button" class="btn btn-primary btn-lg">Submit</button>
+							
+							</div>
+							
+						
+					</div>
+				</div>
+			</div>
+		</section>
+		
+
+
+		
+		<!--Bootstrap jquery -->
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+
+		<!--Bootstrap JavaScript Bundle with Popper -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+
+	</body>
+</html>
